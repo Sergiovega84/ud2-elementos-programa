d7f55a4c085f8a7eae21002960ccab7587a9a667
Sergio Vega <lakio84@gmail.com>
2017-10-16 20:19:37 +0200

    Ejercicio 10
    This reverts commit 4cd29099a736e8246f1dcfa90991988b35313055.

---
+++ 10_temperatura/Ejercicio 10/src/com/sergio_vega/Main.java
@@ -0,0 +1,30 @@
+package com.sergio_vega;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Introduce valor en grados Celsius (ºC): ");
+        double x = Double.parseDouble(br.readLine());
+        System.out.print("Introfuce unidad a la que quiereas convertir (F o K): ");
+        char y = br.readLine().charAt(0);
+
+        if (y == 'F') {
+            x = x * 1.8;
+            System.out.print("El resultado en grados Fahrenheit es: " + x);
+        } else {
+            if (y == 'K') {
+                x = x + 273.15;
+                System.out.print("El resultado en grados Kelvin es: " + x);
+            } else {
+                if (y != 'F' || y != 'K') {
+                    System.out.print("Debes introducir F o K");
+                }
+            }
+        }
+    }
+}
