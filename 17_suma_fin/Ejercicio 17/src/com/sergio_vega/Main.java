fa43b35dd3c395405902ac6ce0b935171812d2f3
Sergio Vega <lakio84@gmail.com>
2017-10-25 20:02:12 +0200

    ejercicio 17

---
+++ 17_suma_fin/Ejercicio 17/src/com/sergio_vega/Main.java
@@ -0,0 +1,34 @@
+package com.sergio_vega;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int total = 0;
+        String x;
+
+
+        do {
+            System.out.print("Introduzca un valor o escriba FIN para terminar: ");
+            x = br.readLine();
+
+            if (!x.equals("FIN")) {
+                total += Integer.parseInt(x);
+                if (Integer.parseInt(x) < min) {
+                    min = Integer.parseInt(x);
+                } if (Integer.parseInt(x) > max) {
+                    max = Integer.parseInt(x);
+                }
+            }
+        } while (!x.equals("FIN"));
+
+        System.out.println("La suma total es " + total + ". El mínimo es " + min + ". El máximo es " + max + ".");
+    }
+}
