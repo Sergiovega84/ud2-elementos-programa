d186650954c3958ed7b863cf64bb85c5aebb2cb9
Sergio Vega <lakio84@gmail.com>
2017-10-25 19:27:24 +0200

    Correcciones

--- 11_acertar_numero/Ejercicio 11/src/com/sergio_vega/Main.java
+++ 11_acertar_numero/Ejercicio 11/src/com/sergio_vega/Main.java
@@ -8,33 +8,36 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Random r = new Random();
-        int aleatorio = r.nextInt(100);
+        int aleatorio = r.nextInt(100) + 1;
+        String respuesta;
+        int n = 0;
         int x = 0;
 
-        System.out.print("Introduce número o FIN: ");
-
-        String respuesta = br.readLine();
-
-        while (!respuesta.equals("FIN")) {
-
-            int n = Integer.parseInt(respuesta);
-            if (n == aleatorio) {
-                System.out.print( "Has acertado, has usado " +x+ " intentos");
-                break;
-            } else if (n > aleatorio) {
-                System.out.print("Te has pasado, ");
-                x = x + 1;
-            }else {
-                System.out.print("Te has quedado corto, ");
-                x = x + 1;
+        do {
+            System.out.print("Escribe: número o FIN ");
+            respuesta = br.readLine();
+            if (!respuesta.equalsIgnoreCase("FIN")) {
+                n = Integer.parseInt(respuesta);
+                if (n == aleatorio) {
+                    System.out.println("Número correcto, ");
+                } else if (n > aleatorio) {
+                    System.out.println("Te has pasado...");
+                } else {
+                    System.out.println("No llegas...");
+                }
+                x = x+ 1;
             }
 
-            System.out.print("Intentalo de nuevo ");
-            respuesta = br.readLine();
+        } while (!respuesta.equalsIgnoreCase("FIN") && n != aleatorio);
+
+        if (n != aleatorio)
+            System.out.println("Hasta la próxima");
+        else {
+            System.out.println("lo has conseguido en " +x+ " intentos");
+
 
         }
-
     }
 }
